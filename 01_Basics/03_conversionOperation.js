a5d8d4558ebf596aad76bfbab7c71b561ec2fe21
Gauatm Patil <gautampatilgj5@gmail.com>
2024-02-22 04:02:05 +0000

    Conversion Operations

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber)
+console.log(valueNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let bool = 1
+let boolcov = Boolean (bool)
+console.log(boolcov);
+
+let num = 33
+let strNum = String(num)
+console.log(strNum);
+console.log(typeof strNum);
