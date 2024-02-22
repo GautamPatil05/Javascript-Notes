64461eb44f30066e16b43df429d757b4f6f92c09
Gauatm Patil <gautampatilgj5@gmail.com>
2024-02-22 03:49:51 +0000

    data types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    +3) // code readability should be high
+
+console.log("Gautam");
+
+let name = "Gautam" // String
+let age = 24 // number
+let isLoggedIn = false // boolean
+
+// number => 2^53
+// bigint => for bigger than int 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => empty
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
