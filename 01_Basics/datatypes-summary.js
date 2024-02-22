ee6f575ac5c167f11a834e6ff045e2acda8f38ba
Gauatm Patil <gautampatilgj5@gmail.com>
2024-02-22 08:55:06 +0000

    datatypes in js

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+// Premitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+// console.log(id == anotherId);
+
+const num = 123
+const num2 = 124.3
+
+const isLogIn = false
+
+const temp = null
+
+const bigNum = 2345657658688n
+
+// Referances (Non Premitive)
+
+// Arrays, Objects, Functions
+
+const heros = ["Iron Man", "Thor", "Hulk"]
+
+myObject = {
+    name: "Gautam",
+    age: 24,
+    gender: "male",
+    
+}
+
+const myFunction = function() {
+    
+    console.log("This is an function");
+}
+
+console.log(typeof anotherId);
+
