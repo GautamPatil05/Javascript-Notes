57f0a67b50807e4301d502a07f317b4fea27e9db
Gauatm Patil <gautampatilgj5@gmail.com>
2024-02-23 18:06:28 +0000

    numbers and math module in js

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,39 @@
+const score = 100
+// console.log(score);
+
+const balance = new Number(200);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2))
+
+const otherNumber = 34.5654
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')); // for indian use 'en-IN'
+
+
+// ****************************** Math ********************************** 
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.644));
+// console.log(Math.ceil(4.644));
+// console.log(Math.floor(4.644));
+// console.log(Math.min(5, 7, 2, 4));
+// console.log(Math.max(5, 7, 2, 4));
+
+console.log(Math.random()); // between 0 to 1
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1); // value from 1 to 
+
+const min = 10
+const max = 20 
+
+console.log( Math.floor( (Math.random() * (max - min + 1) ) + min) )
+// will give the value min 10 to max 20
+
+// refer console, use 'console.log(Math)' to see more function
+
