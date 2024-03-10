0941ad65523fd0a6ffd9173a261cf896e0adc13d
Gauatm Patil <gautampatilgj5@gmail.com>
2024-03-10 16:09:01 +0000

    Objects

--- 02_Basics/03_Objects.js
+++ 02_Basics/03_Objects.js
@@ -3,11 +3,14 @@
 
 // object literals
 
+const mySym = Symbol("key1")
+
 const JSuser = {
 
     name: "Gautam",
     "full name": "Gautam Madhukar Patil",
     age: 24,
+    [mySym]: "mykey1", // syntax of declaring symbol
     location: "Shirpur",
     email: "gautam@tata.com",
     isLoggedIn: false,
@@ -16,6 +19,28 @@ const JSuser = {
 
 // ways to access the data of objects
 
-console.log(JSuser.email);
-console.log(JSuser["email"]);
-console.log(JSuser["full name"]); // cannot access by 'JSuser.full name'
+// console.log(JSuser.email); // way 1 to access object
+// console.log(JSuser["email"]); way 2
+// console.log(JSuser["full name"]); // cannot access by 'JSuser.full name'
+
+// console.log(JSuser[mySym]); // printing symbol value
+// console.log(typeof JSuser[mySym]); // printing symbol value
+
+JSuser.email = "gautam@mahendra.com"
+// console.log(JSuser["email"]);
+
+// Object.freeze(JSuser); // freezes the value of object, not changed if try to change
+JSuser.email = "patil@tata.com"
+// console.log(JSuser);
+
+// function
+JSuser.greeting = function() {
+    console.log("Hello JS Users");
+}
+
+JSuser.greetingTwo = function() {
+    console.log(`Hello JS Users, ${this.name}`);
+}
+
+console.log(JSuser.greeting());
+console.log(JSuser.greetingTwo());
