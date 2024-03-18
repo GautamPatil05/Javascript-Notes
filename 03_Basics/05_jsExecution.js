57c524ff7769eb75b476ed5e42ad5a7be13d2ee0
GautamPatil05 <gautampatilgj5@gmail.com>
2024-03-18 23:23:13 +0530

    js execution

--- 03_Basics/05_jsExecution.js
+++ 03_Basics/05_jsExecution.js
@@ -10,7 +10,7 @@ JavaScript Execution Context
 ? Phases of JS Code execution
 1. Global execution context <- assign to this
 2. Memory Creation Phase -> memory allocated (placed only) to the variables, func's (undefined)
-3. Execution Phase -> executed the func's
+3. Execution Phase -> assign values to variables, executed the func's
 
 ! When function is called it create separate 'new variable environment + execution thread' in which 2nd and 3rd process repeat for each function (separate sand box) and delete after work done
 */
