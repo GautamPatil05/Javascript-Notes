aef70e59dea0227b4aadac0d7f52907644f96a99
Gauatm Patil <gautampatilgj5@gmail.com>
2024-04-05 08:43:44 +0000

    All code of console

---
+++ 06_DOM/two.js
@@ -0,0 +1,16 @@
+/* code from console
+
+document.getElementById('title')
+<h1 id="title" class="heading">This is about the Document Object Model </h1>
+
+document.getElementById('title').className
+'heading'
+
+document.getElementById('title').getAttribute('class')
+'heading'
+
+document.getElementById('title').getAttribute('id')
+'title'
+
+
+*/
